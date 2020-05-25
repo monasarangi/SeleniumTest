9813a658e87da72f5f8f87c45e92d6db31fc6ad5
monasarangi <65875947+monasarangi@users.noreply.github.com>
2020-05-25 14:12:20 -0700

    Add files via upload

---
+++ eBay/src/com/ebay/selenium/automation/pages/BrowsePage.java
@@ -0,0 +1,62 @@
+package com.ebay.selenium.automation.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.LoadableComponent;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BrowsePage extends LoadableComponent<BrowsePage> {
+	public WebDriver driver;
+	public WebElement ebayLogo;
+	public WebElement nextPagination;
+	public List<WebElement> allItemAnchorElements;
+	public static final String URL = "https://www.ebay.com/b/Rolex-Wristwatches/31387/bn_2989578";
+
+	// Default Constructor
+	public BrowsePage(WebDriver driver) {
+		this.driver = driver;
+
+	}
+
+	public WebElement getEbayLogo() {
+		return driver.findElement(By.id("gh-la")); //Find ebay logo identifier
+		//return ebayLogo;
+	}
+
+	public WebElement getNextPagination() {
+		return driver.findElement(By.cssSelector(".ebayui-pagination > a[rel='next']")); //Find pagination > arrow
+//		return nextPagination;
+	}
+
+	public List<WebElement> getAllItemAnchorElements() {
+		return driver.findElements(By.cssSelector("div.container a.s-item__link")); //Find All item links
+//		return allItemAnchorElements;
+	}
+
+	@Override
+	protected void load() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	protected void isLoaded() throws Error {
+		if (!isElementDisplayed(driver, By.cssSelector("div.srp-controls__row-cells"))) {
+			throw new WebDriverException("Browse Page is not successfully loaded");
+		}
+	}
+
+	public static boolean isElementDisplayed(WebDriver driver, By element) {
+		try {
+			new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(element));
+		} catch (WebDriverException ex) {
+			return false;
+		}
+		return true;
+	}
+
+}
---
+++ eBay/src/com/ebay/selenium/automation/tests/WatchTest.java
@@ -0,0 +1,235 @@
+package com.ebay.selenium.automation.tests;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.ebay.selenium.automation.pages.BrowsePage;
+
+public class WatchTest {
+
+	public BrowsePage browsePage;
+	public static WebDriver driver;
+	public WebDriverWait webDriverWait;  
+	  
+
+	Map<String, Integer> map = new HashMap<String, Integer>();
+
+	private static final int MAX_ITEM_COUNT = 250;
+
+	@BeforeClass
+	public void setUp() {
+		//Set chromedriver.exe path 
+		System.setProperty("webdriver.chrome.driver", "/Users/m0s04ai/Documents/chromedriver");
+		//Initialize chrome driver instance
+		driver = new ChromeDriver();
+		browsePage = new BrowsePage(driver);
+		webDriverWait= new WebDriverWait(driver, 15);
+	}
+
+	/**
+	 * Loads browse page And verify eBay logo on top of the page
+	 */
+	@Test(groups = "WatchTest", priority = 1, alwaysRun = true)
+	public void verifyEbayLogo() {
+		driver.get(BrowsePage.URL);
+		driver.manage().window().maximize();
+		Assert.assertTrue(browsePage.getEbayLogo().isDisplayed());
+		Reporter.log("Verified eBay Log Displayed on Page", true);
+	}
+
+	/**
+	 * Get All item links from browse pages using pagination. Check if the item
+	 * has link to ViewItem(ebay.com/itm) , product(ebay.com/p/) or category
+	 * page(ebay.com/c/).Get View Per Hour from each of the pages and ignore if
+	 * any of the page has no "View Per Hour"
+	 *
+	 * Add page URL and no of views per hour to Map<String, Integer> map = new
+	 * HashMap<String, Integer>();
+	 */
+	@Test(groups = "WatchTest", priority = 1, alwaysRun = true)
+	public void printUrlWIthViews() {
+		int pagination = 1;
+		int totalItems = 0;
+		boolean isNextPageEnabled = true;
+
+		do {
+			driver.get(BrowsePage.URL + "?_pgn=" + pagination);
+			webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("gh-la")));
+			driver.manage().window().maximize();
+
+			// Check teh valuee of pagination arrow
+			String paginationDisabled = browsePage.getNextPagination().getAttribute("aria-disabled");
+
+			// Check if Paginations > enabled or not
+			isNextPageEnabled = paginationDisabled == null || !paginationDisabled.equals("true");
+
+			// Get List of Anchor links for each item from browse page item
+			// section
+			List<WebElement> allItemAnchorElements = browsePage.getAllItemAnchorElements();
+			int size = allItemAnchorElements.size();
+			// Check if totalItem+size is exceeding teh MAX_ITEM_COUNT then get
+			// link of remaining items to reach MAX_ITEM_COUNT
+			if ((totalItems + size) > MAX_ITEM_COUNT) {
+				allItemAnchorElements = allItemAnchorElements.subList(0, MAX_ITEM_COUNT - totalItems);
+			}
+
+			totalItems += allItemAnchorElements.size();
+			System.out.println("");
+			// Get All item urls
+			List<String> itemUrls = getItemUrls(allItemAnchorElements);
+
+			// Loop through each of the item link and open in browser
+			for (String viewItemUrl : itemUrls) {
+				// Open item link in browser
+				driver.get(viewItemUrl);
+				// Check the URL
+				System.out.println("URL IS ****"+driver.getCurrentUrl());
+				Assert.assertTrue(driver.getCurrentUrl().contains("ebay.com/itm")
+						|| driver.getCurrentUrl().contains("ebay.com/p/")
+						|| driver.getCurrentUrl().contains("ebay.com/c/"));
+
+				try {
+					WebElement viewPerHourFromPage = null;
+					// Check if the page is VI page
+					if (driver.getCurrentUrl().contains("ebay.com/itm")) {
+						// Get view per hour element for VI paae
+						viewPerHourFromPage = driver.findElement(By.id("vi_notification_new"));
+						// Check if the page is Product page
+					} else if (driver.getCurrentUrl().contains("ebay.com/p/")) {
+						// Get view per hour element for Product Page
+						viewPerHourFromPage = driver.findElement(By.className("banner-status"));
+					}
+
+					if (viewPerHourFromPage != null) { //If item/product has view per hour
+						addViewsToMap(viewPerHourFromPage, viewItemUrl);
+					}
+
+				} catch (NoSuchElementException ex) {
+					// Handle exception If no views present in the page
+				}
+			}
+
+			pagination += 1;
+		} while (totalItems < MAX_ITEM_COUNT && isNextPageEnabled);
+        Reporter.log("Total number of items checked ---- " +totalItems);
+		Reporter.log("Items with views ----" + map.size(), true);
+		Set<String> set = map.keySet();
+		for (String element : set) {
+			Reporter.log(element + " ----views----" + map.get(element));
+		}
+	}
+
+	/**
+	 * This method depends on previous method printUrlWIthViews as teh preevious
+	 * method is adding urls & no of views to Map Sort the map by value and
+	 * print top 5 values
+	 */
+	@Test(groups = "WatchTest", dependsOnMethods = "printUrlWIthViews", priority = 1, alwaysRun = true)
+	public void printTop5Watches() {
+		Map<String, Integer> sortedmap = sortMapByValue();
+		Set<String> set = sortedmap.keySet();
+		int count = 0;
+		Reporter.log("Top 5 Watches with Highest Views below ********* ", true);
+		for (String element : set) {
+			Reporter.log(element + " ---- has Number of Views ----  " + sortedmap.get(element));
+			count++;
+			if (count == 5) {
+				break;
+			}
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param allItemAnchorElements
+	 * @return List of Item anchor links
+	 */
+	private List<String> getItemUrls(List<WebElement> allItemAnchorElements) {
+		List<String> itemUrls = new ArrayList<String>();
+		for (WebElement url : allItemAnchorElements) {
+			itemUrls.add(url.getAttribute("href"));
+
+		}
+
+		return itemUrls;
+	}
+
+	/**
+	 * 
+	 * @param viewPerHourPage
+	 * @param viewItemUrl
+	 *            Extract integer value from <<# viewed per hour>> e.g. 20
+	 *            viewed per hour Add Url and views to Map
+	 */
+	private void addViewsToMap(WebElement viewPerHourPage, String viewItemUrl) {
+		String viewPerHourText;
+		Integer noOfView = null;
+
+		if (viewPerHourPage.isDisplayed()) {
+			
+			//Get text from view per hour element
+			viewPerHourText = viewPerHourPage.getText();
+			System.out.println(viewItemUrl+"----"+viewPerHourText);
+			//Ignore if teh text is Last one else extract the integer value from it
+			if (!viewPerHourText.equalsIgnoreCase("Last one!")) {
+				String viewCount = viewPerHourText.split(" ")[0];
+				noOfView = Integer.parseInt(viewCount);
+				map.put(viewItemUrl, noOfView);
+			}
+		}
+	}
+
+	/**
+	 * Sort map by value
+	 * 
+	 * @return sorted Map by Value
+	 */
+	private Map<String, Integer> sortMapByValue() {
+		List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(map.entrySet());
+
+		// Sort the list
+		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+				return (o2.getValue()).compareTo(o1.getValue());
+			}
+		});
+
+		// put data from sorted list to hashmap
+		HashMap<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+		for (Map.Entry<String, Integer> aa : list) {
+			sortedMap.put(aa.getKey(), aa.getValue());
+		}
+		return sortedMap;
+	}
+
+	/**
+	 * Quit driver or close driver by calling driver.close()
+	 */
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
